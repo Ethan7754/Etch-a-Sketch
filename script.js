628c057d253d0849471e9bcd82f7fe5cc057c722
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-14 16:42:24 -0700

    Add for loop for div creation

---
+++ script.js
@@ -0,0 +1,8 @@
+
+for (let i = 0; i > 15; i++) {
+  const container = document.getElementById("container");
+  const square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
+}
+
