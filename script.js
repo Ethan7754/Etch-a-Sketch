d65e69afd494d12d13c6c539fb8bfd101a961792
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-15 14:58:57 -0700

    FInally generate all square divs

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.getElementById("container");
-const square = document.createElement("div");
-square.classList.add("square");
-square.style.width = "100px";
-square.style.height = "100px";
-square.style.border = "2px solid black";
+
+
 
 for (let i = 0; i < 255; i++) {
+  let square = document.createElement("div");
+  square.classList.add("square");
+  square.style.width = "100px";
+  square.style.height = "100px";
+  square.style.border = "2px solid black";
   container.appendChild(square);
 }
 
