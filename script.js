c6848b62773a4f1dbf03de74585c6faa965a6ba6
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-14 17:02:21 -0700

    Add additional styling to square divs

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const container = document.getElementById("container");
+const square = document.createElement("div");
+square.classList.add("square");
+square.style.width = "100px";
+square.style.height = "100px";
+square.style.border = "2px solid black";
 
-for (let i = 0; i > 15; i++) {
-  const container = document.getElementById("container");
-  const square = document.createElement("div");
-  square.classList.add("square");
+for (let i = 0; i < 255; i++) {
   container.appendChild(square);
 }
 
