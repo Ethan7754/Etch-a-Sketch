c0edec63a062ef9e7be2bc836c91397459230c3b
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-16 16:42:16 -0700

    Create 16x16 square grid

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const container = document.getElementById("container");
 
-
-
-for (let i = 0; i < 255; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
-  square.style.width = "100px";
-  square.style.height = "100px";
-  square.style.border = "2px solid black";
-  container.appendChild(square);
+for (let i = 0; i < 16; i++) {
+  let row = document.createElement("div");
+  row.style.width = "100%";
+  container.appendChild(row);
+  for (let i = 0; i < 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = "50px";
+    square.style.height = "50px";
+    square.style.border = "2px solid black";
+    container.appendChild(square);
+  }
 }
+
+
+
+
+
 
