fe2f79bd0ef9d0dcf42f78f8c220fda894a1746f
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-26 21:59:27 -0700

    Make button display prompt

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const container = document.getElementById("container");
+const button = document.createElement("button");
+button.textContent = "Choose grid-size!";
+
+function displayPrompt() {
+  let userPrompt = prompt("Enter desired grid-size!");
+  alert (userPrompt);
+}
+
+
+button.addEventListener("click", displayPrompt);
+container.appendChild(button);
+let square;
+
+function changeSquareColour() {
+  this.style.backgroundColor = "black";
+}
+
 
 for (let i = 0; i < 16; i++) {
   let row = document.createElement("div");
   row.style.width = "100%";
   container.appendChild(row);
   for (let i = 0; i < 16; i++) {
-    let square = document.createElement("div");
+    square = document.createElement("div");
+    square.addEventListener("mouseover", changeSquareColour);
     square.classList.add("square");
     square.style.width = "50px";
     square.style.height = "50px";
