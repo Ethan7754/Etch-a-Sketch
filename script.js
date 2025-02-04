63628d526b7fa96ad36c51e090c786cebd5c95c7
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-04 06:15:05 -0700

    Finish opacity feature, to best of my abilities

--- script.js
+++ script.js
@@ -11,16 +11,6 @@ container.style.height = "800px";
 container.style.display = "flex";
 container.style.flexDirection = "column";
 
-
-function displayPrompt() {
-  let userPrompt = prompt("Enter desired grid-size!");
-  alert (userPrompt);
-  return userPrompt;
-}
-
-
-
-
 function changeSquareColour() {
   let randomRNum = Math.floor(Math.random() * 255);
   let randomGNum = Math.floor(Math.random() * 255);
@@ -35,6 +25,8 @@ function increaseOpacity() {
   currentOpacity += 0.1; 
   currentOpacity = Math.min(currentOpacity, 1); 
 }
+
+
 
 for (let i = 0; i < 16; i++) {
   let row = document.createElement("div");
@@ -52,30 +44,26 @@ for (let i = 0; i < 16; i++) {
   }
 }
 
-
-
-
-
-function setGridSize(gridSize) {
-  container.style.display = none;
-  for (let i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  row.style.width = "100%";
-  container.appendChild(row);
-  for (let i = 0; i < gridSize; i++) {
-    square = document.createElement("div");
-    square.addEventListener("mouseover", changeSquareColour);
-    square.classList.add("square");
-    square.style.width = "50px";
-    square.style.height = "50px";
-    square.style.border = "2px solid black";
-    container.appendChild(square);
+function displayPrompt() {
+  let userAnswer = prompt("Enter desired grid-size! (any number between 1 and 100 inclusive :)");
+  container.style.display = "none";
+  for (let i = 0; i < userAnswer; i++) {
+    let row = document.createElement("div");
+    container.appendChild(row);
+    row.style.display = "flex";
+    for (let i = 0; i < userAnswer; i++) {
+      square = document.createElement("div");
+      square.addEventListener("mouseover", changeSquareColour);
+      square.addEventListener("mouseover", increaseOpacity);
+      square.classList.add("square");
+      square.style.width = "50px";
+      square.style.height = "50px";
+      square.style.border = "2px solid black";
+      container.appendChild(square);
+    }
   }
+  container.style.display = "initial";
 }
-
-}
-
-
 
 
 
